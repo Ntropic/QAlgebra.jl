073485673439a2cd2c50a2d1af8d4e1d7814643d
Ntropic <21343359+Ntropic@users.noreply.github.com>
2025-04-30 12:55:11 +0200

    changed doc/make

--- docs/make.jl
+++ docs/make.jl
@@ -1,16 +1,10 @@
 using qAlgebra
 using Documenter
 
-DocMeta.setdocmeta!(qAlgebra, :DocTestSetup, :(using qAlgebra); recursive=true)
 
 makedocs(;
     modules=[qAlgebra],
-    authors="Michael Schilling",
-    sitename="qAlgebra.jl",
-    format=Documenter.HTML(;
-        edit_link="main",
-        assets=String[],
-    ),
+    sitename="qAlgebra.jl Documentation",
     pages=[
         "Home" => "index.md",
         "API Reference" => [
